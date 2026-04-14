45165cd0a9f332f2cf5c6edfb8a13aae61ecf3b6
brayozin <bryan.fellipe.sc@gmail.com>
2026-04-14 13:55:20 -0300

    chore: ignore local build and agent folders

--- .next-prod/static/chunks/app/layout-460fc942b94f70e1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7177],{552:()=>{},3065:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,552,23))}},e=>{e.O(0,[7317,1751,2579,7358],()=>e(e.s=3065)),_N_E=e.O()}]);
